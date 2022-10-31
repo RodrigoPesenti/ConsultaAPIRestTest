3950debbfea699b961cbb156e46977f58a707337
Rodri <Rodri@DESKTOP-060V4TJ>
2022-10-31 01:13:51 -0300

    Agregar archivos de proyecto.

---
+++ ConsultaAPIRestTest/ConsultasAPI.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsultaAPIRestTest
+{
+    internal class ConsultasAPI
+    {
+        readonly RestClient _client;
+
+        public ConsultasAPI()
+        {
+            _client = new RestClient("http://[::1]:3000")
+                .AddDefaultHeader(KnownHeaders.Accept, "application/json");
+        }
+
+        public Usuario? GetUsuarioPorID(string ID)
+        {
+            return _client.GetJson<Usuario>("/usuarios/" + ID);
+        }
+
+        public RestResponse PostUsuario(Usuario usuario)
+        {
+            string jsonString = JsonSerializer.Serialize(usuario);
+            var request = new RestRequest("/usuarios");
+            request.AddJsonBody(jsonString);
+            return _client.Post(request);
+        }
+    }
+}
---
+++ ConsultaAPIRestTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsultaAPIRestTest;
+using RestSharp;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            ConsultasAPI ca = new ConsultasAPI();
+
+            //Usuario? usuario = ca.GetUsuarioPorID("6350asc696fd8c3f65c09872f5");
+            //Console.WriteLine(usuario.Nombre);
+
+            Usuario usuario = new Usuario("Marcos", "1254125", "Av. test");           
+            ca.PostUsuario(usuario);
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+    }
+}
